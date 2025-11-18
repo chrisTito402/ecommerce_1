6f949ad0625758f67de50e0a6917cec394ed0507
DanielBuelnaAndujo <daniel.buelna260378@potros.itson.edu.mx>
2025-11-18 00:09:05 -0700

    Persistencia

---
+++ ecommercePersistencia/src/main/java/itson/org/ecommercepersistencia/EcommercePersistencia.java
@@ -0,0 +1,15 @@
+package itson.org.ecommercepersistencia;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author daniel
+ */
+public class EcommercePersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
