22ec6b190cfe8ff0ef44f16c8895339dc7a1eee2
DanielBuelnaAndujo <daniel.buelna260378@potros.itson.edu.mx>
2025-11-18 10:43:23 -0700

    Metodos para consultar Productos

--- ecommercePersistencia/src/main/java/definiciones/IProductoDAO.java
+++ ecommercePersistencia/src/main/java/definiciones/IProductoDAO.java
@@ -11,4 +11,7 @@ import java.util.List;
 public interface IProductoDAO {
     public boolean registrarProducto(Producto producto) throws PersistenciaException;
     public List<Producto> obtenerProductos() throws PersistenciaException;
+    public boolean actualizarProducto(Producto producto) throws PersistenciaException;
+    public Producto obtenerProducto(String nombre) throws PersistenciaException;
+    public Producto obtenerProducto(Long id) throws PersistenciaException;
 }
--- ecommercePersistencia/src/main/java/implementaciones/ProductoDAO.java
+++ ecommercePersistencia/src/main/java/implementaciones/ProductoDAO.java
@@ -25,7 +25,6 @@ public class ProductoDAO implements IProductoDAO {
             return true;
         } catch (Exception e) {
             em.getTransaction().rollback();
-            System.out.println("Rollback en persist");
             throw new PersistenciaException("Error al registrar Producto");
         } finally {
             em.close();
@@ -43,6 +42,59 @@ public class ProductoDAO implements IProductoDAO {
             return query.getResultList();
         } catch(Exception e) {
             throw new PersistenciaException("Error al consultar la Lista de Productos");
+        } finally {
+            em.close();
+        }
+    }
+    
+    @Override
+    public boolean actualizarProducto(Producto producto) throws PersistenciaException {
+        EntityManager em = Conexion.crearConexion();
+        try {
+            em.getTransaction().begin();
+            em.merge(producto);
+            em.getTransaction().commit();
+            
+            return true;
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new PersistenciaException("Error al actualizar Producto");
+        } finally {
+            em.close();
+        }
+    }
+    
+    @Override
+    public Producto obtenerProducto(String nombre) throws PersistenciaException {
+        EntityManager em = Conexion.crearConexion();
+        try {
+            String jpql = "SELECT pi FROM Producto pi WHERE "
+                    + "pi.nombre = :nombre";
+            TypedQuery<Producto> query = em.createQuery(jpql, Producto.class);
+            query.setParameter("nombre", nombre);
+            query.setMaxResults(1);
+            List<Producto> producto = query.getResultList();
+            
+            if (producto.isEmpty()) {
+                return null;
+            } else {
+                return producto.getFirst();
+            }
+        } catch(Exception e) {
+            throw new PersistenciaException("Error al consultar Producto.");
+        } finally {
+            em.close();
+        }
+    }
+    
+    @Override
+    public Producto obtenerProducto(Long id) throws PersistenciaException {
+        EntityManager em = Conexion.crearConexion();
+        try {
+            Producto producto = em.find(Producto.class, id);
+            return producto;
+        } catch(Exception e) {
+            throw new PersistenciaException("Error al consultar Producto.");
         } finally {
             em.close();
         }
